ac157a329ca1aa32746ea1fd5068998ab2d412a9
Drachev Danila <tmdonoy@gmail.com>
2022-09-01 19:41:17 +0300

    first commit

---
+++ src/Mili.java
@@ -0,0 +1,13 @@
+public class Mili {
+    public static void main(String[] args) {
+        int ticket = 1000; // стоимость билета
+        int bonus = 20; // 1 бонусная миля
+        int mili = ticket / bonus;
+
+                System.out.println("Количство миль за билет");
+                System.out.println(mili + " миль");
+
+            }
+        }
+
+
